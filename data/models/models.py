802a6f613ef0f6042ee0a29157e741197c0e80c7
Coconnuts <luxtor124@gmail.com>
2025-07-23 20:25:11 +0000

    added the models

--- data/models/models.py
+++ data/models/models.py
@@ -1,7 +1,10 @@
+import sys
+import os
 import pandas as pd
 import glob
 from prediction_ai import train_model
-
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+from prediction_ai import train_model
 # Step 1: Load and concatenate all CSVs
 files = glob.glob("data/raw/*.csv")
 dfs = [pd.read_csv(f) for f in files]
