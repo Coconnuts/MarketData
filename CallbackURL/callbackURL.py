ed1f380621609f8c5052ff56d89645c1a9f6da13
Coconnuts <luxtor124@gmail.com>
2025-07-28 19:22:07 +0000

    fixed callback to work

--- CallbackURL/callbackURL.py
+++ CallbackURL/callbackURL.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -8,4 +9,6 @@ def receive():
     return "GET received!", 200
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8443, ssl_context=('cert.pem', 'key.pem'))
+    cert_path = os.path.join(os.path.dirname(__file__), 'cert.pem')
+    key_path = os.path.join(os.path.dirname(__file__), 'key.pem')
+    app.run(host='127.0.0.1', port=8443, ssl_context=(cert_path, key_path))
